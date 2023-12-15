dff6625d0083c4b2e4c5f8c01137c9cf8ce43eed
mccbabii <mi2cn8cs@gmail.com>
2023-12-15 10:43:31 -0500

    Update Home and add animation

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,57 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../animatedLetters/AnimatedLetters'
 import "../../styles/home.scss";
 
 const Home = () => {
-  return <div>Hello World</div>;
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = [' ', 'C', 'h', 'r', 'i', 's', 't', 'y']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Full-stack Developer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
 };
 
 export default Home;
