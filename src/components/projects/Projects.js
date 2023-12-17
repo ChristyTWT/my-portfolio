4c98fe27648cd6beac99affa7190b613f6c6ff1a
mccbabii <mi2cn8cs@gmail.com>
2023-12-17 11:09:18 -0500

    Updated Projects page

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,27 @@
 import "../../styles/projects.scss";
 
 const Projects = () => {
-  return <div className="container">Hello World</div>;
+  return (
+    <div className="container">
+      <div className="container-projects content-margin">
+        <div className="content-heading">
+          <div className="text-heading">My Projects</div>
+          <div className="text-content">
+            I Love to Craft Beautiful and Scaleable Web Product
+          </div>
+        </div>
+        <div className="projects-content">
+          <div className="projects-list">
+            <div className="projects-item">1</div>
+            <div className="projects-item">1</div>
+            <div className="projects-item">1</div>
+            <div className="projects-item">1</div>
+            <div className="projects-item">1</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Projects;
