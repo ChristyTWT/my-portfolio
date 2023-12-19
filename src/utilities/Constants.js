2df7af6b2448cd5ed48ae06d7333077b4320ea2d
mccbabii <mi2cn8cs@gmail.com>
2023-12-19 06:36:13 -0500

    Added Const for skills, porjects, and contact page

--- src/utilities/Constants.js
+++ src/utilities/Constants.js
@@ -1,2 +1,36 @@
+
+// Skills Page
 export const SKILLS_LIST = [['HTML',90],['CSS',95],['jQuery',90],['SEO',90],['Java',85],['Rest Api',90],['JavaScript',95],['Python',60]];
 
+
+// Skills Page
+export const SKILLS_HEADING = 'My Skills';
+export const SKILLS_CONTENT = 'I Love to Craft Beautiful and Scaleable Web Product';
+export const SKILLS_A_HEADING = 'Design + Development';
+export const SKILLS_A_CONTENT = 'Clean, odern design, obtimized for performance, search engine and converting user to customers.';
+export const SKILLS_B_HEADING = 'Technology';
+export const SKILLS_B_CONTENT = 'Combined all the latest technology to a progressive website.'
+export const SKILLS_C_HEADING = 'Always Responsive';
+export const SKILLS_C_CONTENT = ' A responsive design makes your website accessible to all user, regardless to their device.'
+
+
+// Projects Page
+export const PROJECTS_HEADING = 'My Projects';
+export const PROJECTS_CONTENT = 'I Love to Craft Beautiful and Scaleable Web Product';
+
+
+// Contact Page
+
+// Emailjs 
+export const EMAILJS_SERVICE_ID = 'service_hs4067j';
+export const EMAILJS_TEMPLATE_ID = 'template_g7608qg';
+export const EMAILJS_PUBLIC_KEY = 'wDu0vy6w9ODoNHATQ';
+
+export const CONTACT_HEADING = 'Ready to Get Started ?';
+export const CONTACT_CONTENT = 'Fill in the form to get started !';
+export const CONTACT_FORM_HEADING = 'Send Me a Message!';
+export const CONTACT_FORM_MSG = 'Message';
+export const CONTACT_FORM_NAME = 'Your name..';
+export const CONTACT_FORM_EMAIL = 'Your email..';
+
+export const BUTTON_SEND = 'Send';
