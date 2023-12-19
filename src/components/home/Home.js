f34e8379a8d13eba2b5e76e460dafa98fba54214
mccbabii <mi2cn8cs@gmail.com>
2023-12-19 06:37:22 -0500

    Add responsive design to all pages

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
     '.',
   ]
   return (
-    <div className="container home-page">
+    <div className="container">
       <div className="home-margin text-zone">
         <div className="text-heading">
           <span className={letterClass}>H</span>
